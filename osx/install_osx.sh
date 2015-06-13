c7c4545ef3256bba166db443020f311a9547ffcf
Shreedhar Hardikar <hardikar@cs.wisc.edu>
2015-06-13 15:54:29 -0700

    Moved mac -> osx

---
+++ osx/install_osx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "installing homebrew"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "brewing all the things"
+# source osx/brew.sh
+
+echo "updating OSX settings"
+# source osx/osx.sh
+
