27c2fa75f1751efe88d22c58296a2647a89c8d0d
Shreedhar Hardikar <hardikar@cs.wisc.edu>
2015-06-13 18:50:49 -0700

    Added zshrc and zsh is ready to be usable

---
+++ zsh/aliases.sh
@@ -0,0 +1,36 @@
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+    colorflag="--color"
+else # OS X `ls`
+    colorflag="-G"
+fi
+
+
+# Filesystem aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+
+alias l="ls -lah ${colorflag}"
+alias la="ls -AF ${colorflag}"
+alias ll="ls -lFh ${colorflag}"
+alias lld="ls -l | grep ^d"
+alias rmf="rm -rf"
+
+alias grep='grep --color=auto'
+
+
+# Git aliases
+alias gcam="git commit -am"
+alias gcm="git commit -m"
+alias gsu="git submodule update"
+alias gs="git status"
+alias gl="git log"
+alias gc="git checkout"
+alias glog="git log --graph --decorate --all"
+
+# Enable 256-color more by default
+alias tmux="tmux -2"
+
