356837216f570737dbdb40a6f4e5f4e2635f2edd
Shreedhar Hardikar <hardikar@cs.wisc.edu>
2015-09-12 01:49:17 -0700

    Got basic window management set up with Slate config

--- osx/karabiner-import.sh
+++ osx/karabiner-import.sh
@@ -6,8 +6,12 @@ $cli set remap.holdcommandQ 1
 /bin/echo -n .
 $cli set repeat.initial_wait 200
 /bin/echo -n .
-$cli set bilalh.remap.f19_escape_control 1
-/bin/echo -n .
 $cli set parameter.keyoverlaidmodifier_timeout 300
+/bin/echo -n .
+$cli set private.ctrl_to_hyper 1
+/bin/echo -n .
+$cli set private.vi_arrows 1
+/bin/echo -n .
+$cli set bilalh.remap.f19_escape_control 1
 /bin/echo -n .
 /bin/echo
