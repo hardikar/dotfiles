f9d26812089f661098d16e618138c022ea9f6fed
Shreedhar Hardikar <hardikar@cs.wisc.edu>
2015-09-19 20:26:47 -0700

    Completed work on tile-tabs-control.js - tabed browsing here I come!

--- .vimperator/plugin/tile-tabs-control.js
+++ .vimperator/plugin/tile-tabs-control.js
@@ -1,10 +1,15 @@
 /*
  *  Vimperator commands and mappings for Tile Tabs Firefox plugins
  *
- *  TODO :
- *  Also C-W C[HJKL]
+ *  Mappings :
+ *  <C-W><C-W>      cycle through windows in the same tab
+ *  <C-W>j          select pane below
+ *  <C-W>k          select pane above
+ *  <C-W>h          select pane left
+ *  <C-W>l          select pane right
+ *  <C-W>z          Zoom out selected pane
  *
- *  Read about VIM moving windows - and implement some of them.. 
+ *  Added commands vsplit, split, syncscroll
  *
  * @author hardikar
  */
@@ -17,28 +22,34 @@ if (typeof tileTabs != 'object') {
     return;
 }
 
-function SplitAndOpen(args){
+function SplitAndOpenBelow(args){
     liberator.execute("emenu Tile.Tile New Tab.Below");
     liberator.execute("open " + args);
 }
+function SplitAndOpenAbove(args){
+    liberator.execute("emenu Tile.Tile New Tab.Above");
+    liberator.execute("open " + args);
+}
 
-function VerticalSplitAndOpen(args){
+function VerticalSplitAndOpenRight(args){
     liberator.execute("emenu Tile.Tile New Tab.Right");
     liberator.execute("open " + args);
 }
 
-
-function SyncScrolling(args){
-    liberator.execute("syncscroll emenu Tile.Sync Scroll");
+function VerticalSplitAndOpenLeft(args){
+    liberator.execute("emenu Tile.Tile New Tab.Left");
+    liberator.execute("open " + args);
 }
 
-function getAllChildPanelsInLayout(layout) {
+function SyncScrolling(args){
+    liberator.execute("emenu Tile.Sync Scroll");
+}
+
+function getAllChildPanelsFromTile(rootTile) {
     var allChildPanels = [];
 
-    if (! layout) return allChildPanels;
-
-    var stack = []
-    stack.push(layout.rootTile); 
+    var stack = [];
+    stack.push(rootTile);
     while(stack.length >= 1) {
         var tile = stack.pop();
         for(var i = 0; i < tile.childTiles.length; i+=1){
@@ -48,24 +59,10 @@ function getAllChildPanelsInLayout(layout) {
             allChildPanels.push(tile);
         }
     }
-    return allChildPanels; 
+    return allChildPanels;
 }
 
-
-// Cycle through tiles in the same tab
-function CycleTiles(){
-    // Get all child panels by traversing the tree for the current layout
-    var selectedLayout = tileTabs.lastActiveLayout;
-    var allChildPanels = getAllChildPanelsInLayout(selectedLayout);
-
-    // Get the index of the current tab among the child panels
-    var selectedTab = tileTabs.lastSelectedTab;
-    var currentTileIndex = allChildPanels.map(function (e) e.panelID)
-            .indexOf(selectedTab.linkedPanel);
-
-    // Compute the panelID of the next tab
-    var nextTileIndex = (currentTileIndex + 1) % allChildPanels.length;
-    var nextPanelId = allChildPanels[nextTileIndex].panelID;
+function SelectBufferByPanelId(panelId) {
 
     // Get panelIDs of all open tabs mapped to their index
     var openTabPanelIdsToIndexMap = {};
@@ -74,34 +71,93 @@ function CycleTiles(){
 
 
     // Find out the next buffer's index we want to change to
-    var nextIndex = openTabPanelIdsToIndexMap[nextPanelId];
+    var index = openTabPanelIdsToIndexMap[panelId];
 
-    if (nextIndex) {
-        liberator.execute("buffer! " + (nextIndex+1));
+    if (index) {
+        liberator.execute("buffer! " + (index+1));
     }
-    // Do nothing if we failed
+}
+
+function mod(n, m) {
+    return ((n % m) + m) % m;
+
 }
 
 
-// Select with direction in mind
-// direction in ['up', 'down', 'left', 'right']
-function SelectTileVertical(direction){
+// Cycle through tiles in the same tab
+function CycleTiles(){
     // Get all child panels by traversing the tree for the current layout
     var selectedLayout = tileTabs.lastActiveLayout;
-    var selectedTab = tileTabs.lastSelectedTab;
+    if (! layout)
+        return;
 
+    var allChildPanels = getAllChildPanelsFromTile(selectedLayout.rootTile);
+
+    // Get the index of the current tab among the child panels
+    var selectedTab = tileTabs.lastSelectedTab;
+    var currentTileIndex = allChildPanels.map(function (e) e.panelID)
+            .indexOf(selectedTab.linkedPanel);
+
+    // Compute the panelID of the next tab
+    var nextTileIndex = mod(currentTileIndex - 1, allChildPanels.length);
+    alert(nextTileIndex);
+    var nextPanelId = allChildPanels[nextTileIndex].panelID;
+
+    SelectBufferByPanelId(nextPanelId);
+}
+
+
+// Internal method that does the tree traversal
+// watchType in ['hsplit', 'vsplit']
+// d in [-1, 1]
+function SelectTileInternal(watchType, d){
+    // Setup current layout, tab, tile
+    var selectedLayout = tileTabs.lastActiveLayout;
+    var selectedTab = tileTabs.lastSelectedTab;
     var selectedTile = tileTabs.findTileByPanel(selectedLayout.rootTile, selectedTab.linkedPanel);
 
-    alert(selectedTile);
+    // Traverse back up the tree to find the "correct" kind of parent.
+    var tile = selectedTile;
+    while (tile) {
+        var parentTile = tile.parentTile;
+        if ( parentTile.type == watchType ){
+            //Bingo
+            break;
+        } else {
+            tile = parentTile;
+        }
+    }
+    if (!tile || !parentTile){
+        // We failed, return quietly
+        return;
+    } else {
+        var currentTileIndex = parentTile.childTiles.indexOf(tile);
+
+        var nextTileIndex = mod(currentTileIndex + d, parentTile.childTiles.length);
+        var nextTile = parentTile.childTiles[nextTileIndex];
+
+        var nextPanelId = "";
+        if (nextTile.type != 'panel') {
+            // oh well we must list all out child panels then
+            var allChildPanels = getAllChildPanelsFromTile(nextTile);
+            // ff we find any, just grab that panelID
+            nextPanelId = allChildPanels.length >= 1 ? allChildPanels[0].panelID : "";
+        }else {
+            // our sibling is a panel
+            nextPanelId = nextTile.panelID;
+        }
+
+        SelectBufferByPanelId(nextPanelId);
+    }
 }
 
 
 // Select with direction in mind
 // direction in ['up', 'down', 'left', 'right']
 function SelectTile(direction){
-    alert(direction == 'down');
-    if (direction == 'up' || direction == 'down')
-        SelectTileVertical(direction);
+    var d = (direction == 'down' || direction == 'right') ? 1 : -1;
+    var watchType = (direction == 'up' || direction == 'down') ? 'hsplit': 'vsplit';
+    SelectTileInternal(watchType, d);
 }
 
 
@@ -110,9 +166,16 @@ commands.addUserCommand(
     ['sp[lit]', 'sp'],
     "Open in a horizontal split window",
     function(args, bang, count) {
-        SplitAndOpen(args);
+        if ("-above" in args)
+            SplitAndOpenAbove(args);
+        else
+            SplitAndOpenBelow(args);
     },
     { /* extras */
+        options: [
+            [["-above"], commands.OPTION_NOARG],
+            [["-below"], commands.OPTION_NOARG]
+        ],
         completer: function (context) completion.url(context, "l"),
         argCount: "*",
         bang: false,
@@ -121,15 +184,22 @@ commands.addUserCommand(
     },
     true /* replace */
 );
-    
+   
 // Vertical split command
 commands.addUserCommand(
     ['vs[plit]', 'vsp', 'vs'],
     "Open in a vertical split window",
     function(args, bang, count) {
-        VerticalSplitAndOpen(args);
+        if ("-left" in args)
+            VerticalSplitAndOpenLeft(args);
+        else
+            VerticalSplitAndOpenRight(args);
     },
     { /* extras */
+        options: [
+            [["-left"], commands.OPTION_NOARG],
+            [["-right"], commands.OPTION_NOARG]
+        ],
         completer: function (context) completion.url(context, "l"),
         argCount: "*",
         bang: false,
@@ -156,33 +226,32 @@ commands.addUserCommand(
     true /* replace */
 );
 
-//command! cycletiles js CycleTiles()
 
-mappings.addUserMap([modes.NORMAL], 
-        ['<C-W><C-W>'], 
-        '', 
+mappings.addUserMap([modes.NORMAL],
+        ['<C-W><C-W>'],
+        '',
         function () {
             CycleTiles();
         }
     );
-mappings.addUserMap([modes.NORMAL], 
-        ['<C-W><C-V>'], 
-        '', 
+mappings.addUserMap([modes.NORMAL],
+        ['<C-W><C-V>'],
+        '',
         function () {
             VerticalSplitAndOpen("");
         }
     );
-mappings.addUserMap([modes.NORMAL], 
-        ['<C-W><C-S>'], 
-        '', 
+mappings.addUserMap([modes.NORMAL],
+        ['<C-W><C-S>'],
+        '',
         function () {
             SplitAndOpen("");
         }
     );
 
-mappings.addUserMap([modes.NORMAL], 
-        ['<C-W>j'], 
-        '', 
+mappings.addUserMap([modes.NORMAL],
+        ['<C-W>j'],
+        '',
         function () {
             SelectTile("down");
         },
@@ -190,5 +259,47 @@ mappings.addUserMap([modes.NORMAL],
             noremap: true
         }
     );
-//nnoremap <C-W><C-W> :cycletiles<CR>
+mappings.addUserMap([modes.NORMAL],
+        ['<C-W>k'],
+        '',
+        function () {
+            SelectTile("up");
+        },
+        { /* extras */
+            noremap: true
+        }
+    );
+mappings.addUserMap([modes.NORMAL],
+        ['<C-W>h'],
+        '',
+        function () {
+            SelectTile("left");
+        },
+        { /* extras */
+            noremap: true
+        }
+    );
+mappings.addUserMap([modes.NORMAL],
+        ['<C-W>l'],
+        '',
+        function () {
+            SelectTile("right");
+        },
+        { /* extras */
+            noremap: true
+        }
+    );
+
+
+mappings.addUserMap([modes.NORMAL],
+        ['<C-W>z'],
+        '',
+        function () {
+            liberator.execute("emenu Tile.Expand Tile");
+        },
+        { /* extras */
+            noremap: true
+        }
+    );
+
 })();
