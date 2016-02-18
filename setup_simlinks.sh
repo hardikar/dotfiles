bc9e0cbbbb8631803e19d4fd932c350fbbaa2a6b
Shreedhar Hardikar <shardikar@pivotal.io>
2016-02-18 11:27:51 -0800

    setup_simlinks

---
+++ setup_simlinks.sh
@@ -0,0 +1,22 @@
+if [ -e "$1" ]; then
+    echo "Enter the path to setup the simlinks"
+fi
+
+OUTDIR="$1"
+INDIR="$(pwd)"
+
+ln -s $INDIR/.vimrc $OUTDIR
+ln -s $INDIR/.vim $OUTDIR
+
+ln -s $INDIR/.tmux.conf $OUTDIR
+ln -s $INDIR/.config $OUTDIR
+
+ln -s $INDIR/.zshrc $OUTDIR
+ln -s $INDIR/.zsh $OUTDIR
+ln -s $INDIR/.bashrc $OUTDIR
+
+ln -s $INDIR/.vimperator $OUTDIR
+ln -s $INDIR/.vimperatorrc $OUTDIR
+
+ln -s $INDIR/bin $OUTDIR
+
