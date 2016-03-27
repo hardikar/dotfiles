b91f807a0d290100dd5bd3621a1a68bea7967c24
Shreedhar Hardikar <shardikar@pivotal.io>
2016-03-27 10:35:08 -0700

    Started deb.sh

---
+++ install/deb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get update && apt-get upgrade
+apt-get install -y build-essential
+
+apt-get install -y apt-file
+
+apt-get install -y font-inconsolata
+apt-get install -y vim-nox git tmux tree
+
