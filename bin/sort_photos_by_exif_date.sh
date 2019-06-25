ebc27b82ca6905897afdca319174abf14d28eef2
Shreedhar Hardikar <hardikar@cs.wisc.edu>
2019-06-25 10:19:22 -0500

    Fixup sort_photos

--- bin/sort_photos_by_exif_date.sh
+++ bin/sort_photos_by_exif_date.sh
@@ -6,8 +6,10 @@
 DIR=${1:-`pwd`}
 
 find "$DIR" -type f -maxdepth 1 | while read f; do
-  if file -i "$f" | grep -q image; then
-    date=`exif -t 'DateTime' -m "$f"`
+  echo -n "$file"
+  if file --mime-type "$f" | grep -q image; then
+    date=`exif --tag 'DateTime' --machine-readable "$f"`
+    echo "$date"
     if test $? -eq 0; then
       # EXIF data is present
       date=`echo "$date" | cut -c 1-10 | sed 's/:/-/g'`
