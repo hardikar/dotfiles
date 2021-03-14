8fc96edadd740a7a7cf2cc7f1b7911bdc9072136
Shreedhar Hardikar <shardikar@vmware.com>
2021-03-13 23:22:03 -0600

    Revive .sh/exports.sh that was not indexed in git

--- .sh/exports.sh
+++ .sh/exports.sh
@@ -1,2 +1,18 @@
-export EDITOR=vim
-export PAGER=less
+# export other environment variables
+export PAGER='less'
+export MANPAGER='less'
+export EDITOR='vim'
+
+# PATH="/usr/texbin"
+# PATH="/sbin:$PATH"
+# PATH="/usr/sbin:$PATH"
+# PATH="/bin:$PATH"
+# PATH="/usr/bin:$PATH"
+# PATH="/usr/local/bin:$PATH"
+
+export PATH
+
+# check for custom bin directory and add to path
+if [[ -d $HOME/bin ]]; then
+	export PATH=~/bin:$PATH
+fi
