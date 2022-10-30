0c9ca0a502f8f1f67061a35cc00895934a572d19
Shreedhar Hardikar <hardikar@cs.wisc.edu>
2022-10-30 15:55:19 -0700

    Support both Linux & MacOS in sort_photos_by_exif_date

--- bin/sort_photos_by_exif_date.sh
+++ bin/sort_photos_by_exif_date.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-set -o errexit -o errtrace -o pipefail -o nounset
+set -o nounset
 
 # Requires exif to be installed on the system!
 if ! command -v exif 1>&2 2>/dev/null; then
@@ -8,19 +8,33 @@ if ! command -v exif 1>&2 2>/dev/null; then
 	exit 1
 fi
 
+case $(uname) in
+	'Linux')
+		MV='mv --backup=numbered --no-clobber'
+		;;
+	'Darwin')
+		MV='mv -n'
+		;;
+	*)
+		echo "Unsupported OS: $(uname)"
+		exit 1
+		;;
+esac
+
 DIR=${1:-`pwd`}
 echo "Searching in $DIR"
 
 find "$DIR" -maxdepth 1 -type f | while read f; do
 	if file --mime-type "$f" | grep -q image; then
+		echo -n "$f -> "
 		date=$(exif --tag 'DateTime' --machine-readable "$f")
-		echo "$f -> $date"
-		if test $? -eq 0; then
+		rc="$?"
+		echo "$date"
+		if [ $rc -eq 0 ]; then
 			# EXIF data is present
 			date=`echo "$date" | cut -c 1-10 | sed 's/:/-/g'`
-
 			mkdir -p "$date"
-			mv --backup=numbered --no-clobber "$f" "$date/"
+			$MV "$f" "$date/"
 		fi
 	fi
 done
